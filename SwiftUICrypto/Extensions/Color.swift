a02777981b0a7d31c7d247b1d77b9e13badff30b
Siarhei Ramaniuk <s.m.ramaniuk@gmail.com>
2024-05-26 16:53:53 +0200

    Add color themes extension

---
+++ SwiftUICrypto/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  SwiftUICrypto
+//
+//  Created by Siarhei Ramaniuk on 26.05.24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+
+    static let theme = ColorTheme()
+
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
